08e56e1eb6be760b70544f08d444a4b2b1083d9a
Dongyue Chen <dongyue@Dongyues-MBP.local>
2017-01-23 14:31:51 +0700

    Hello world

---
+++ MakeATry/MakeATry/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MakeATry
+//
+//  Created by Dongyue Chen on 1/23/17.
+//  Copyright © 2017 Dongyue Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello world!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
